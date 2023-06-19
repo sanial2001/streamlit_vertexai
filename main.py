77e644eac2713933a1c9c2b09388e084acaca4a0
saniald.int <saniald.int@media.net>
2023-06-19 20:14:29 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+import json
+import streamlit as st
+import requests
+import sys
+
+
+def run():
+    option = st.selectbox(
+        'Select the type of generative AI?',
+        ('Text Prompt', 'Chat Prompt', 'Fine Tune Prompt'), )
+    st.write('You selected:', option)
+
+    if option == 'Text Prompt':
+        input_text = st.text_input(label="Enter the input text")
+        data = {
+            "input_text": input_text,
+            "input_type": option
+        }
+        if st.button("output"):
+            resp = requests.post("http://172.16.200.30:10179/vertex", json=data)
+            print(resp.text)
+            json_data = json.loads(resp.text)
+            st.json(json_data)
+
+    elif option == 'Chat Prompt':
+        input_text = st.text_input(label="Enter the input text")
+        data = {
+            "input_text": input_text,
+            "input_type": option
+        }
+        if st.button("output"):
+            resp = requests.post("http://172.16.200.30:10179/vertex", json=data)
+            print(resp.text)
+            json_data = json.loads(resp.text)
+            st.json(json_data)
+
+
+if __name__ == '__main__':
+    run()
